0f5da24f1105b07656ece6cda0ae26400605398c
queenriwon <queenriwon3@gmail.com>
2025-02-28 21:09:46 +0900

    Init

---
+++ src/test/java/com/example/deliveryappproject/DeliveryAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
