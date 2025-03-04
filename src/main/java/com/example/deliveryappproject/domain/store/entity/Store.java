0de59bb3fa5cde34ab1c6cbecde390996817a38c
queenriwon <queenriwon3@gmail.com>
2025-03-04 18:25:29 +0900

    Test(store): StoreServiceTest 보완작성

--- src/main/java/com/example/deliveryappproject/domain/store/entity/Store.java
+++ src/main/java/com/example/deliveryappproject/domain/store/entity/Store.java
@@ -3,6 +3,7 @@ package com.example.deliveryappproject.domain.store.entity;
 import com.example.deliveryappproject.common.entity.Timestamped;
 import com.example.deliveryappproject.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import static jakarta.persistence.EnumType.STRING;
 @Getter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "stores")
 public class Store extends Timestamped {
 
@@ -42,6 +44,14 @@ public class Store extends Timestamped {
         this.user = user;
         this.storeName = storeName;
         this.storeState = StoreState.ACTIVE;
+        this.openAt = openAt;
+        this.closeAt = closeAt;
+        this.minOrderPrice = minOrderPrice;
+    }
+
+    public Store(Long id, String storeName, LocalTime openAt, LocalTime closeAt, BigDecimal minOrderPrice) {
+        this.id = id;
+        this.storeName = storeName;
         this.openAt = openAt;
         this.closeAt = closeAt;
         this.minOrderPrice = minOrderPrice;
