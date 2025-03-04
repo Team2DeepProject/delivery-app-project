fdb5429e225c75c5eeb03c1712c10756d11bb1cf
JoeMinKyung <mk981015@naver.com>
2025-03-04 21:49:40 +0900

    Feat(review): NotFoundException 추가

---
+++ src/main/java/com/example/deliveryappproject/common/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.deliveryappproject.common.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
